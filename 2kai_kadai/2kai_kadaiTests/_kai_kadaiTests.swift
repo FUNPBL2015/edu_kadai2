dae3137b057d673a7cf4128ee1ebd69ab2d83477
Youhei Shinpo <b1013220@fun.ac.jp>
2015-05-26 16:48:53 +0900

    課題をpushする

---
+++ 2kai_kadai/2kai_kadaiTests/_kai_kadaiTests.swift
@@ -0,0 +1,36 @@
+//
+//  _kai_kadaiTests.swift
+//  2kai_kadaiTests
+//
+//  Created by 新保遥平 on 2015/05/22.
+//  Copyright (c) 2015年 Yohei Shinpo. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class _kai_kadaiTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
