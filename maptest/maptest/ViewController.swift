e56748475ac3d3c02556e2e987e5796f227617fa
Youhei Shinpo <b1013220@fun.ac.jp>
2015-05-26 16:53:22 +0900

    課題をpushする

--- maptest/maptest/ViewController.swift
+++ maptest/maptest/ViewController.swift
@@ -2,73 +2,79 @@
 //  ViewController.swift
 //  maptest
 //
-//  Created by Seiya Kurokome on 2015/05/20.
-//  Copyright (c) 2015年 Seiya Kurokome. All rights reserved.
+//  Created by 新保遥平 on 2015/05/20.
+//  Copyright (c) 2015年 Yohei Shinpo. All rights reserved.
 //
 
 import UIKit
 import MapKit
-class ViewController: UIViewController , MKMapViewDelegate , CLLocationManagerDelegate{
+class ViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate{
     
     @IBOutlet weak var myMapView: MKMapView!
-    var myLocationManaeger: CLLocationManager!
+    var myLocationManager: CLLocationManager!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
+        var myURL = NSURL(string:"http://www.fun.ac.jp/")
+        
+        
         self.myMapView?.delegate = self
+        
         var region:MKCoordinateRegion = self.myMapView!.region
         var location:CLLocationCoordinate2D
-        = CLLocationCoordinate2DMake(41.842011, 140.766987)
+        = CLLocationCoordinate2DMake(41.842011,140.766987)
         region.center = location
         region.span.latitudeDelta = 0.005
         region.span.longitudeDelta = 0.005
         
         self.myMapView!.setRegion(region, animated: true)
         
-        myLocationManaeger = CLLocationManager()
+        myLocationManager = CLLocationManager()
         
-        myLocationManaeger.delegate = self
-        myLocationManaeger.distanceFilter = kCLDistanceFilterNone
-        myLocationManaeger.desiredAccuracy = kCLLocationAccuracyHundredMeters
+        myLocationManager.delegate = self
+        myLocationManager.distanceFilter = kCLHeadingFilterNone
+        myLocationManager.desiredAccuracy =kCLLocationAccuracyHundredMeters
         
-        myLocationManaeger.startUpdatingLocation()
+        myLocationManager.startUpdatingLocation()
         
         var myPin: MKPointAnnotation = MKPointAnnotation()
         myPin.coordinate = location
         myPin.title = "公立はこだて未来大学"
         myPin.subtitle = "亀田中野町116番地2"
+        
         myMapView.addAnnotation(myPin)
+        
+        // 長押しのUIGestureRecognizerを生成.
+        var myLongPress: UILongPressGestureRecognizer = UILongPressGestureRecognizer()
+        
+        myLongPress.addTarget(self, action: "recognizeLongPress:")
+        
+        // MapViewにUIGestureRecognizerを追加.
+        
+        myMapView.addGestureRecognizer(myLongPress)
+        
     }
     
-    func locationManager(manager: CLLocationManager!, didUpdateLocations locations: [AnyObject]!) {
+    func recognizeLongPress(sender: UILongPressGestureRecognizer) {
+        
+        
+        let url = "http://www.fun.ac.jp/"
+        let targetURL = NSURL(string: url)
+        UIApplication.sharedApplication().openURL(targetURL!)//Safariでとばす
+        
+    }
+    
+    func locationManager(manager: CLLocationManager!, didUpdateLocations locations:[AnyObject]!) {
+        
+        
         myMapView.showsUserLocation = true
     }
     
-    func mapView(mapView: MKMapView!, viewForAnnotation annotation: MKAnnotation!) -> MKAnnotationView! {
-        let reuseId = "pin"
-        
-        var pinView = mapView.dequeueReusableAnnotationViewWithIdentifier(reuseId) as? MKPinAnnotationView
-        if pinView == nil{
-            pinView = MKPinAnnotationView(annotation: annotation, reuseIdentifier: reuseId)
-            pinView!.canShowCallout = true
-            pinView!.animatesDrop = true
-            pinView!.pinColor = .Purple
-            
-            var rightButton: AnyObject! = UIButton.buttonWithType(UIButtonType.DetailDisclosure)
-            pinView!.rightCalloutAccessoryView = rightButton as? UIView
-        }
-        else {
-            pinView!.annotation = annotation
-        }
-        return pinView
-        }
-
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
 }
 
